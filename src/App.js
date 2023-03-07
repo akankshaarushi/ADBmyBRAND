f4e8345bd61ca0d5dd8f85fa2366f0d77ddc8ae1
akankshaarushi <115232645+akankshaarushi@users.noreply.github.com>
2023-03-07 19:05:10 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Argument from '../src/Components/Argument';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Argument/>
+    </div>
+  );
+}
+
+export default App;
